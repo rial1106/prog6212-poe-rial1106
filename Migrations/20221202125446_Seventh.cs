a7193258b3decf01be10919960d4e3bb6aa10f0a
Rial Seebran <ST10083702@vcconnect.edu.za>
2022-11-29 15:47:36 +0200

    New Migrations.

---
+++ Migrations/20221202125446_Seventh.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seventh : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Module_User_username",
+                table: "Module");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "username",
+                table: "Module",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Module_User_username",
+                table: "Module",
+                column: "username",
+                principalTable: "User",
+                principalColumn: "username",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Module_User_username",
+                table: "Module");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "username",
+                table: "Module",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Module_User_username",
+                table: "Module",
+                column: "username",
+                principalTable: "User",
+                principalColumn: "username");
+        }
+    }
+}
