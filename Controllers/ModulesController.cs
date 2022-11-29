9e553335d9383698050bd540de42821f46eecf28
Rial Seebran <ST10083702@vcconnect.edu.za>
2022-11-29 12:48:24 +0200

    Add project files.

---
+++ Controllers/ModulesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Study_Tracker.Data;
+using Study_Tracker.Models;
+
+namespace Study_Tracker.Controllers
+{
+    public class ModulesController : Controller
+    {
+        private readonly Study_TrackerContext _context;
+
+        public ModulesController(Study_TrackerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Modules
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Module.ToListAsync());
+        }
+
+        // GET: Modules/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Module == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Module
+                .FirstOrDefaultAsync(m => m.moduleCode == id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return View(@module);
+        }
+
+        // GET: Modules/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Modules/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("moduleCode,moduleName,credits,classHoursPerWeek,hoursStudiedToday,hoursStudiedThisWeek,semesterNumOfWeeks,semesterStartDate")] Module @module)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@module);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@module);
+        }
+
+        // GET: Modules/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Module == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Module.FindAsync(id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+            return View(@module);
+        }
+
+        // POST: Modules/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("moduleCode,moduleName,credits,classHoursPerWeek,hoursStudiedToday,hoursStudiedThisWeek,semesterNumOfWeeks,semesterStartDate")] Module @module)
+        {
+            if (id != @module.moduleCode)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@module);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModuleExists(@module.moduleCode))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@module);
+        }
+
+        // GET: Modules/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Module == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Module
+                .FirstOrDefaultAsync(m => m.moduleCode == id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return View(@module);
+        }
+
+        // POST: Modules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Module == null)
+            {
+                return Problem("Entity set 'Study_TrackerContext.Module'  is null.");
+            }
+            var @module = await _context.Module.FindAsync(id);
+            if (@module != null)
+            {
+                _context.Module.Remove(@module);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModuleExists(string id)
+        {
+          return _context.Module.Any(e => e.moduleCode == id);
+        }
+    }
+}
---
+++ Models/Module.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Study_Tracker.Models
+{
+    public class Module
+    {
+
+        [Key]
+        [MaxLength(50)]
+        public string moduleCode { get; set; } = null!; // Module code.
+        [MaxLength(50)]
+        public string moduleName { get; set; } = null!; // Module name.
+        public int credits { get; set; } // Module credits.
+        public double classHoursPerWeek { get; set; } // The number of class hours there are for a week.
+        public double? hoursStudiedToday { get; set; } // How many hours of self studying were done today.
+        public double? hoursStudiedThisWeek { get; set; } // How many hours you should self study as per the formula.
+        public int semesterNumOfWeeks { get; set; } // How many weeks in a semester
+        public DateTime semesterStartDate { get; set; } // When does the semester starts
+        [NotMapped]
+        public double? percentageCompleted { get; set; } // Percentage of hours studied towards recommendedStudyHours for the ProgressBar.
+
+        public ICollection<StudyDate> studyDates { get; set; } = null!;
+    }
+}
