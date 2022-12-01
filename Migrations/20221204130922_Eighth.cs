cb2f1e41f210783ffb0c1fb1871ddd59badc2418
Rial Seebran <ST10083702@vcconnect.edu.za>
2022-12-01 16:07:29 +0200

    Updated Module Schema.

---
+++ Migrations/20221204130922_Eighth.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Eighth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "hoursStudiedToday",
+                table: "Module");
+
+            migrationBuilder.RenameColumn(
+                name: "hoursStudiedThisWeek",
+                table: "Module",
+                newName: "HoursStudiedThisWeek");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HoursStudiedThisWeek",
+                table: "Module",
+                newName: "hoursStudiedThisWeek");
+
+            migrationBuilder.AddColumn<double>(
+                name: "hoursStudiedToday",
+                table: "Module",
+                type: "float",
+                nullable: true);
+        }
+    }
+}
---
+++ Migrations/20221204132638_Ninth.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ninth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "RecommendedStudyHours",
+                table: "Module",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecommendedStudyHours",
+                table: "Module");
+        }
+    }
+}
