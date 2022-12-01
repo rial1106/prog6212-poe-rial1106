02348d1f1cd7aef71c935b8c3a0638d2ae7f7cf1
Rial Seebran <ST10083702@vcconnect.edu.za>
2022-12-01 16:10:30 +0200

    Login now the default page.

--- Program.cs
+++ Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Accounts}/{action=Register}/{id?}");
+    pattern: "{controller=Accounts}/{action=Login}/{id?}");
 
 app.Run();
